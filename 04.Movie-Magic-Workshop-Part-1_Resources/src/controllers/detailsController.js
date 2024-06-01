526d82c72ee710a5c97c858b0e11a3908a837443
Stefan Sabev <stevenbg91@gmail.com>
2024-06-01 07:28:36 +0200

    Add details view

---
+++ 04.Movie-Magic-Workshop-Part-1_Resources/src/controllers/detailsController.js
@@ -0,0 +1,5 @@
+module.exports = {
+    detailsController: (req, res) => {
+        res.render('details');
+    }
+}
--- 04.Movie-Magic-Workshop-Part-1_Resources/src/routes.js
+++ 04.Movie-Magic-Workshop-Part-1_Resources/src/routes.js
@@ -1,10 +1,12 @@
 const {Router} = require('express');
 const { homeController } = require('./controllers/homeController');
 const { createControllerGet } = require('./controllers/createController');
+const {detailsController} = require('./controllers/detailsController');
 
 const router = Router();
 
 
 router.get("/",homeController);
 router.get("/create",createControllerGet);
+router.get("/details/:id",detailsController)
 module.exports = {router};
