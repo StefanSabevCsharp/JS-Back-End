ac2738bd10bd5c7ff82f9ea4c6fd865e04d12069
Stefan Sabev <stevenbg91@gmail.com>
2024-06-22 09:25:46 +0200

    add user model and auth service working properly

---
+++ Exam Prep 2/src/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    
+    email:{
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: [true, 'Password is required'],
+    },
+}, {timestamps: true});
+
+userSchema.pre("save",async function() {
+    this.password = await bcrypt.hash(this.password, 11);
+});
+
+// userSchema.virtual("repass")
+// .set(function(value){
+//     if(value !== this.password){
+//         throw new Error("Passwords don't match");
+//     }
+// });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ Exam Prep 2/src/services/authService.js
@@ -0,0 +1,50 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("../lib/jwt");
+const SECRET = require("../lib/jwt").SECRET;
+
+exports.register = async (userData) => {
+  
+    if (userData.password !== userData.repass) {
+        throw new Error("Passwords do not match");
+    } 
+    const user = await User.findOne({ email: userData.email });
+    if (user) {
+        throw new Error("User already exists");
+    }
+    
+    const createdUser = await User.create(userData);
+    const token = await generateToken(createdUser);
+    
+    return token; 
+};
+
+exports.login = async (userData) => {
+    const { email, password } = userData;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error("Invalid email or password");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new Error("Invalid email or password");
+    }
+   return generateToken(user);
+
+};
+
+function generateToken(userData){
+    const id = userData._id;
+    const payLoad = {
+        _id: id,
+        username: userData.username,
+        email: userData.email,
+    };
+    
+    //TO DO: have to export the sectet in deffirent file
+    const token = jwt.sign(payLoad, SECRET, { expiresIn: "2h" });
+    return token;
+}
