31f5a1bb50450ccad9a5c2af8838d1908dd0a0b2
Stefan Sabev <stevenbg91@gmail.com>
2024-07-04 18:09:05 +0200

    Index.js and jwt custom functions

---
+++ Regular Exam/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const { configExpress } = require("./config/configExpress");
+const { configHandlebars } = require("./config/configHandlebars");
+const mongoose = require("mongoose");
+const router = require("../routes");
+
+
+const app = express();
+configExpress(app);
+configHandlebars(app);
+
+async function start() {
+    await mongoose.connect("mongodb://localhost:27017/regularExam").then(console.log("Connected to DB"));
+    app.listen(3000, () => console.log("Server is listening on port 3000"));
+};
+
+start();
